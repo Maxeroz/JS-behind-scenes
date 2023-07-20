b50b6d26c7fcb2976dfe79b8921fbdaf9cd1d686
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-07-20 18:05:44 +0400

    Scoping in Practice

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function calcAge(birtYear) {
+  const age = 2037 - birtYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birtYear}`;
+    console.log(output);
+
+    if (birtYear >= 1981 && birtYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
