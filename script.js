439aacd3fa46b1e63bc903fe326cfd12eed464c6
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-07-21 23:20:11 +0400

    Regular Functions vs. Arrow Functions

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ function calcAge(birtYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 
-*/
-
 // Variables
 console.log(me);
 // console.log(job);
@@ -73,3 +71,86 @@ var numProducts = 10;
 function deleteShoppingCar() {
   console.log('All products deleted!');
 }
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+// f();
+*/
+
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+jonas.greet();
+jonas.calcAge();
+
+const addExp = function (a, b) {
+  console.log(arguments);
+  return arguments[2];
+};
+addExp(2, 5, 8, 12);
+
+console.log();
+
+let addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+
+addArrow(2, 5, 8);
