cd8fd63e8f3452b2ec3b8e8b7a8085c0e1894a51
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-07-22 14:28:39 +0400

    Primitives vs. Objects in Practice

--- script.js
+++ script.js
@@ -107,7 +107,6 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 // f();
-*/
 
 // var firstName = 'Matilda';
 
@@ -154,3 +153,58 @@ let addArrow = (a, b) => {
 };
 
 addArrow(2, 5, 8);
+
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log('Friend', friend);
+console.log('Me', me);
+*/
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firtName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage:', marriedJessica);
+
+// Copying objects
+
+const jessica2 = {
+  firtName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
